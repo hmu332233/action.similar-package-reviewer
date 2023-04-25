6edfff7869b3c5c984ec167140cd7d2c9fc7f974
Minung Han <hmu332233@gmail.com>
2023-04-26 02:34:57 +0900

    summary에 header 제거

--- src/summaryHelper.ts
+++ src/summaryHelper.ts
@@ -1,8 +1,5 @@
 import * as core from '@actions/core';
 
 export async function writeSummary(results: string) {
-  return await core.summary
-    .addHeading('Package Similarity Results')
-    .addRaw(results)
-    .write();
+  return await core.summary.addRaw(results).write();
 }
