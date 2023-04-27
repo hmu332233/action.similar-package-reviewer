26f8f4aa96d42d5624cd54ea01b9ca72456d4a48
Minung Han <hmu332233@gmail.com>
2023-04-28 03:05:48 +0900

    chatCompletion 사용방식 개선

--- src/openaiHelper.ts
+++ src/openaiHelper.ts
@@ -1,6 +1,11 @@
 import * as core from '@actions/core';
 import { Configuration, OpenAIApi } from 'openai';
 
+const SYSTEM_PROMPT = `Please compare the packages in lists A and B, and identify any with same purposes.
+If a similar package is found, please present the information in the following JSON format:
+[ { "pkgName1": "[package from A list]", "pkgName2": "[package from B list]", "description": "[brief explanation of the similarity]" }]
+If no similar packages are found, output "[]". Please ensure only the JSON format is provided in the response.`;
+
 /**
  * Creates a prompt for OpenAI API to compare packages from two lists and identify any with similar purposes.
  * @param {string[]} pkgNames1 - The first list of package names.
@@ -8,13 +13,9 @@ import { Configuration, OpenAIApi } from 'openai';
  * @returns {string} A formatted prompt to be used with OpenAI API.
  */
 function createPrompt(pkgNames1: string[], pkgNames2: string[]) {
-  return `Please compare the packages in lists A and B, and identify any with similar purposes. Ensure that the response only contains relevant information and no extraneous details. If a similar package is found, please present the information in the following JSON format:
-[ { "pkgName1": "[package from A list]", "pkgName2": "[package from B list]", "description": "[brief explanation of the similarity]" }]
-If no similar packages are found, output "[]". Please ensure only the JSON format is provided in the response.
-  
-Example lists:
-A List: ["${pkgNames1.join('","')}"]
-B List: ["${pkgNames2.join('","')}"]`;
+  return `
+A List: ${pkgNames1.join(', ')}
+B List: ${pkgNames2.join(', ')}`;
 }
 
 /**
@@ -39,7 +40,10 @@ export async function comparePackagesUsingOpenAI(
 
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages: [{ role: 'user', content }],
+    messages: [
+      { role: 'system', content: SYSTEM_PROMPT },
+      { role: 'user', content },
+    ],
   });
 
   const comparedPkgs = JSON.parse(
