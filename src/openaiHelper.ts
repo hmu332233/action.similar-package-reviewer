3f52ea028637ccb0818c602f6a78b47c8ffce672
Sain-Tech <chyh1900@gmail.com>
2023-05-02 12:28:59 +0900

    feat: 시스템 프롬프트 업데이트

--- src/openaiHelper.ts
+++ src/openaiHelper.ts
@@ -1,10 +1,12 @@
 import * as core from '@actions/core';
 import { Configuration, OpenAIApi } from 'openai';
 
-const SYSTEM_PROMPT = `Please compare the packages in lists A and B, and identify any with same purposes.
-If a similar package is found, please present the information in the following JSON format:
+const SYSTEM_PROMPT = `Both list A and B are node module packages.
+Please compare the packages in lists A and B, and find the replaceable packages.
+The replaceable package means similar and alternative package.
+If replaceable packages are found, please present the information in the following JSON format:
 [ { "pkgName1": "[package from A list]", "pkgName2": "[package from B list]", "description": "[brief explanation of the similarity]" }]
-If no similar packages are found, output []. Please ensure only the JSON format is provided in the response.`;
+If no replaceable packages are found, output []. Please ensure only the JSON format is provided in the response.`;
 
 /**
  * Creates a prompt for OpenAI API to compare packages from two lists and identify any with similar purposes.
